43fc9c5a559fa35062d5f4521f1f785a985309ee
mimmarry <105546871+mimmarry@users.noreply.github.com>
2022-10-03 18:08:48 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,22 +1,19 @@
 ﻿using System;
 namespace Triangle;
 class Programm
-{
-    static void Main(string[] args)
+{static void Main(string[] args)
     {
         Console.WriteLine("Введите высоту треугольника");
         int height = int.Parse(Console.ReadLine());
         for (int i = 0; i < height; i++)
         {
-            Console.SetCursorPosition(height - i, i + 1);
             for (int j = 0; j <= i; j++)
             {
-                
                 Console.Write("*");
             }
             Console.WriteLine();
         }
-        Console.ReadKey();
+Console.ReadKey();
     }
 
 
